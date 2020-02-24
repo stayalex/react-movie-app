77a9a497327a1ad06c4760f8fb02cd0f123c7916
stayalexiou <stayalexiou@gmail.com>
2020-02-24 17:47:12 +0200

    movie react-hook-app

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <header className="App-header">
-      <h2>{props.text}hs</h2>
+      <h2>{props.text}</h2>
     </header>
   );
 };
